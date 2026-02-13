9970222e2ba038a5e703bdb1d92d8b9e5c905c0b
Mariam S. <sikhuashvilimariami@gmail.com>
2026-02-13 17:06:46 +0400

    add cn utility with clsx and tailwind merge

--- src/components/ui/SectionBadge.tsx
+++ src/components/ui/SectionBadge.tsx
@@ -1,7 +1,8 @@
+import { twMerge } from "tailwind-merge";
 
 export default function SectionBadge({ icon, text, className }:{ icon?: React.ReactNode, text: string, className?: string }) {
     return (
-        <div className={`px-4 py-2 border border-primary/30 rounded-full bg-primary/10 my-6 w-fit text-primary text-sm font-medium flex flex-row items-center justify-center ${className}`}>
+        <div className={twMerge(`px-4 py-2 border border-primary/30 rounded-full bg-primary/10 my-6 w-fit text-primary text-sm font-medium flex flex-row items-center justify-center`, className)}>
             {icon && <span className="mr-2">{icon}</span>}
             {text}
         </div>
